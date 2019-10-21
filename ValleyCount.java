91cdb3851a471a7ce955312a290ff4fde0da38f4
utkarshnovborn <utkarshmishra067@gmail.com>
2019-10-21 22:20:47 +0530

    Day1:ValleyCount

---
+++ ValleyCount.java
@@ -0,0 +1,29 @@
+
+//        Problem Statement:
+//        https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+public class ValleyCount {
+
+    public static void main(String[]args){
+        System.out.println(valleyCount("UDDDUDUU"));
+    }
+
+    private static int valleyCount(String s) {
+        int valleyCount = 0;
+        int sum = 0;
+        if(s.length()<2){
+            return valleyCount;
+        }
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='D' || s.charAt(i)=='d'){
+                sum -= 1;
+            }else if(s.charAt(i)=='U' || s.charAt(i)=='u'){
+                sum += 1;
+                if(sum==0){
+                    valleyCount+=1;
+                }
+            }
+        }
+        return valleyCount;
+    }
+
+}
